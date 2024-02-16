80d8ded062a000e14a662b7177ff75c4b42c693d
Vitor Brito <joaovitor16611661@gmail.com>
2024-02-15 21:37:59 -0300

    exerceio typescript

---
+++ exercicio.ts
@@ -0,0 +1,17 @@
+function multiplicaNumeros (numero1:number, numero2:number):number {
+    return numero1 * numero2;
+}
+
+const resultado:number = multiplicaNumeros(21,23);
+
+
+
+function dizOla ( nome:string): string {
+    return `Olá ${nome}`;
+}
+
+const saudacao:string = dizOla("Maria");
+
+
+console.log(resultado);
+console.log(saudacao);
